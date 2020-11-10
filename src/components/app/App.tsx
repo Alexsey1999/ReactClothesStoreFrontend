84a3734520ec11a631d091370e25a3d02448ef33
Alexsey Shishkin <shishkinalexsey42@gmail.com>
2020-11-10 18:04:02 +0300

    Making home page

---
+++ src/components/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../home'
+
+import './App.scss'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Home />
+    </div>
+  )
+}
+
+export default App
